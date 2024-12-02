bed3e140be8f86bb60c10fa8fbdc2474bd963fbc
lisssq <lizynka05@yandex.ru>
2024-12-02 15:04:13 +0300

    начало визуальной части

--- Graphics-EmptyProject-master/MyForm.h
+++ Graphics-EmptyProject-master/MyForm.h
@@ -1,4 +1,5 @@
-﻿#pragma once
+﻿
+#pragma once
 
 namespace CppWinForm1 {
 
@@ -23,7 +24,7 @@ namespace CppWinForm1 {
 
 
 
-		void Draw()
+		/*void Draw()
 		{
 			int Start = 360 * CircleQueue->GetHeadIndex() / CircleQueue->GetMaxSize();
 			int Finish = 360 * CircleQueue->GetCurrentSize() / CircleQueue->GetMaxSize();
@@ -42,7 +43,7 @@ namespace CppWinForm1 {
 			MyClearPen->Width = 10.0F;
 			Graphics^ gr = this->CreateGraphics();
 			gr->DrawArc(MyClearPen, CenterX, CenterY, W, H, Start, Finish);
-		}
+		}*/
 
 
 
@@ -71,13 +72,24 @@ namespace CppWinForm1 {
 			}
 		}
 	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::Button^ button2;
+	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::Label^ label2;
+	private: System::Windows::Forms::Label^ label3;
+	private: System::Windows::Forms::Label^ label4;
+	private: System::Windows::Forms::Label^ label5;
+	private: System::Windows::Forms::TextBox^ textBox1;
+	private: System::Windows::Forms::TextBox^ textBox2;
+	private: System::Windows::Forms::Label^ label6;
+	private: System::Windows::Forms::Label^ label7;
+	private: System::Windows::Forms::Label^ label8;
 	protected:
 
 	private:
 		/// <summary>
 		/// Required designer variable.
 		/// </summary>
-		System::ComponentModel::Container ^components;
+		System::ComponentModel::Container^ components;
 
 #pragma region Windows Form Designer generated code
 		/// <summary>
@@ -87,37 +99,176 @@ namespace CppWinForm1 {
 		void InitializeComponent(void)
 		{
 			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->label4 = (gcnew System::Windows::Forms::Label());
+			this->label5 = (gcnew System::Windows::Forms::Label());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
+			this->label6 = (gcnew System::Windows::Forms::Label());
+			this->label7 = (gcnew System::Windows::Forms::Label());
+			this->label8 = (gcnew System::Windows::Forms::Label());
 			this->SuspendLayout();
 			// 
 			// button1
 			// 
-			this->button1->Location = System::Drawing::Point(204, 117);
+			this->button1->Font = (gcnew System::Drawing::Font(L"Comic Sans MS", 10.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->button1->Location = System::Drawing::Point(562, 47);
 			this->button1->Name = L"button1";
-			this->button1->Size = System::Drawing::Size(75, 23);
+			this->button1->Size = System::Drawing::Size(85, 33);
 			this->button1->TabIndex = 0;
-			this->button1->Text = L"button1";
+			this->button1->Text = L"Старт";
 			this->button1->UseVisualStyleBackColor = true;
-			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
+			// 
+			// button2
+			// 
+			this->button2->Font = (gcnew System::Drawing::Font(L"Comic Sans MS", 10.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->button2->Location = System::Drawing::Point(674, 47);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(85, 33);
+			this->button2->TabIndex = 1;
+			this->button2->Text = L"Стоп";
+			this->button2->UseVisualStyleBackColor = true;
+			// 
+			// label1
+			// 
+			this->label1->BackColor = System::Drawing::SystemColors::Control;
+			this->label1->Font = (gcnew System::Drawing::Font(L"Comic Sans MS", 10.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label1->Location = System::Drawing::Point(12, 141);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(283, 33);
+			this->label1->TabIndex = 2;
+			this->label1->Text = L"Максимальная длина очереди:";
+			this->label1->TextAlign = System::Drawing::ContentAlignment::MiddleCenter;
+			// 
+			// label2
+			// 
+			this->label2->Font = (gcnew System::Drawing::Font(L"Comic Sans MS", 10.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label2->Location = System::Drawing::Point(12, 186);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(283, 33);
+			this->label2->TabIndex = 3;
+			this->label2->Text = L"Вероятность добавления: ";
+			this->label2->TextAlign = System::Drawing::ContentAlignment::MiddleCenter;
+			// 
+			// label3
+			// 
+			this->label3->Font = (gcnew System::Drawing::Font(L"Comic Sans MS", 10.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label3->Location = System::Drawing::Point(12, 231);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(283, 33);
+			this->label3->TabIndex = 4;
+			this->label3->Text = L"Вероятность удаления:";
+			this->label3->TextAlign = System::Drawing::ContentAlignment::MiddleCenter;
+			// 
+			// label4
+			// 
+			this->label4->Font = (gcnew System::Drawing::Font(L"Comic Sans MS", 10.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label4->Location = System::Drawing::Point(12, 276);
+			this->label4->Name = L"label4";
+			this->label4->Size = System::Drawing::Size(283, 33);
+			this->label4->TabIndex = 5;
+			this->label4->Text = L"Добавлено элементов:";
+			this->label4->TextAlign = System::Drawing::ContentAlignment::MiddleCenter;
+			// 
+			// label5
+			// 
+			this->label5->Font = (gcnew System::Drawing::Font(L"Comic Sans MS", 10.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label5->Location = System::Drawing::Point(12, 321);
+			this->label5->Name = L"label5";
+			this->label5->Size = System::Drawing::Size(283, 33);
+			this->label5->TabIndex = 6;
+			this->label5->Text = L"Удалено элементов: ";
+			this->label5->TextAlign = System::Drawing::ContentAlignment::MiddleCenter;
+			// 
+			// textBox1
+			// 
+			this->textBox1->Font = (gcnew System::Drawing::Font(L"Comic Sans MS", 10.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->textBox1->Location = System::Drawing::Point(298, 141);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(85, 33);
+			this->textBox1->TabIndex = 7;
+			// 
+			// textBox2
+			// 
+			this->textBox2->Font = (gcnew System::Drawing::Font(L"Comic Sans MS", 10.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->textBox2->Location = System::Drawing::Point(298, 186);
+			this->textBox2->Name = L"textBox2";
+			this->textBox2->Size = System::Drawing::Size(85, 33);
+			this->textBox2->TabIndex = 8;
+			// 
+			// label6
+			// 
+			this->label6->Font = (gcnew System::Drawing::Font(L"Comic Sans MS", 10.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label6->Location = System::Drawing::Point(298, 231);
+			this->label6->Name = L"label6";
+			this->label6->Size = System::Drawing::Size(85, 33);
+			this->label6->TabIndex = 9;
+			this->label6->Text = L"label6";
+			this->label6->TextAlign = System::Drawing::ContentAlignment::MiddleCenter;
+			// 
+			// label7
+			// 
+			this->label7->Font = (gcnew System::Drawing::Font(L"Comic Sans MS", 10.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label7->Location = System::Drawing::Point(298, 276);
+			this->label7->Name = L"label7";
+			this->label7->Size = System::Drawing::Size(85, 33);
+			this->label7->TabIndex = 10;
+			this->label7->Text = L"label7";
+			this->label7->TextAlign = System::Drawing::ContentAlignment::MiddleCenter;
+			// 
+			// label8
+			// 
+			this->label8->Font = (gcnew System::Drawing::Font(L"Comic Sans MS", 10.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label8->Location = System::Drawing::Point(298, 321);
+			this->label8->Name = L"label8";
+			this->label8->Size = System::Drawing::Size(85, 33);
+			this->label8->TabIndex = 11;
+			this->label8->Text = L"label8";
+			this->label8->TextAlign = System::Drawing::ContentAlignment::MiddleCenter;
 			// 
 			// MyForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(731, 434);
+			this->ClientSize = System::Drawing::Size(910, 539);
+			this->Controls->Add(this->label8);
+			this->Controls->Add(this->label7);
+			this->Controls->Add(this->label6);
+			this->Controls->Add(this->textBox2);
+			this->Controls->Add(this->textBox1);
+			this->Controls->Add(this->label5);
+			this->Controls->Add(this->label4);
+			this->Controls->Add(this->label3);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->label1);
+			this->Controls->Add(this->button2);
 			this->Controls->Add(this->button1);
 			this->Name = L"MyForm";
 			this->Text = L"MyForm";
 			this->ResumeLayout(false);
+			this->PerformLayout();
 
 		}
 #pragma endregion
-	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e)
-	{
+	
 
 
-		
 
-			
-	}
+
 	};
 }
